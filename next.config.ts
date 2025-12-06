57ad96edd15010e3fe7e14dc463da98bfd52a382
urvikalal <urvi.qodic@gmail.com>
2025-12-06 17:21:33 +0530

    done

--- next.config.ts
+++ next.config.ts
@@ -44,35 +44,18 @@ const nextConfig: NextConfig = {
   
 
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
     formats: ['image/webp', 'image/avif'],
   },
 
-  webpack: (config, { dev, isServer }) => {
-    // Optimize bundle size
-    if (!dev && !isServer) {
-      config.optimization = {
-        ...config.optimization,
-        splitChunks: {
-          chunks: 'all',
-          cacheGroups: {
-            vendor: {
-              test: /[\\/]node_modules[\\/]/,
-              name: 'vendors',
-              chunks: 'all',
-            },
-            mui: {
-              test: /[\\/]node_modules[\\/]@mui[\\/]/,
-              name: 'mui',
-              chunks: 'all',
-            },
-          },
-        },
-      };
-    }
-
-    return config;
-  },
+  // Empty turbopack config to acknowledge we're using Turbopack
+  // Turbopack handles optimizations automatically
+  turbopack: {},
 };
 
 export default nextConfig;
